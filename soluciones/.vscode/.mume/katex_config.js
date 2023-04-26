3fb05bc93a1e5d23eba14b7231ee0c3dcb44942e
Lautaro Bagnasco <lbagnasco@matchfin.ar>
2023-04-26 13:32:43 -0300

    Hago unos ejercicios probando las macros

--- soluciones/.vscode/.mume/katex_config.js
+++ soluciones/.vscode/.mume/katex_config.js
@@ -1,17 +1,18 @@
 
 module.exports = {
   macros: {
-    "\\true": "\\text{true}",
-    "\\false": "\\text{false}",
-    "\\bool": "\\text{Bool}",
+    "\\true": "\\sf{true}",
+    "\\false": "\\sf{false}",
+    "\\bool": "\\sf{Bool}",
     "\\num": "\\underline{#1}",
-    "\\succ": "\\text{succ}({#1})",
-    "\\pred": "\\text{pred}({#1})",
-    "\\iszero": "\\text{isZero}({#1})",
+    "\\succ": "\\sf{succ}({#1})",
+    "\\pred": "\\sf{pred}({#1})",
+    "\\iszero": "\\sf{isZero}({#1})",
     "\\asign": "\\{{#1}\\leftarrow{#2}\\}",
-    "\\nat": "\\text{Nat}",
-    "\\if": "\\text{if }{#1}\\text{ then }{#2}\\text{ else }{#3}",
-    "\\lda": "\\lambda{#1}:{#2}.\\ {#3}",
+    "\\nat": "\\sf{Nat}",
+    "\\if": "\\sf{if }{#1}\\sf{ then }{#2}\\sf{ else }{#3}",
+    "\\lda": "\\lambda {#1} : {#2} . \\ {#3}",
+    "\\fix": "\\sf{fix}\\ ",
     "\\deriv": "\\frac{#2}{#1}(\\text{#3})",
     "\\t":"\\vdash",
   }
---
+++ soluciones/macros.md
@@ -0,0 +1,8 @@
+# Macros
+
+Para usar las macros definidas en `katex_config.js`:
+
+1. Instalar la extensión `markdown-preview-enhanced`
+2. En la configuración de la extensión, poner Config Path como `${workspaceFolder}/.vscode/.mume`
+3. `Ctrl+Shift+P` -> `Markdown Preview Enhanced: Open KaTeX Config` te abre un archivo llamado `katex_config.js`
+4. Pegarle el contenido de mi archivo `soluciones/.vscode/.mume/katex_config.js`
